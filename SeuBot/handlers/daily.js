1f5c743fe52da5c9a4425c1944aa551073a29e2c
bailey555 <kookj5508@gmail.com>
2025-11-24 19:11:03 -0300

    Create daily.js

---
+++ SeuBot/handlers/daily.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getUser, saveDB } = require('../../handlers/dbManager');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('daily')
+        .setDescription('Receba seu prêmio diário!'),
+
+    async execute(interaction) {
+        const user = getUser(interaction.user.id);
+
+        const amount = 200;
+        user.money += amount;
+        saveDB();
+
+        await interaction.reply(`🎉 Você recebeu **${amount} moedas** hoje!`);
+    }
+};
