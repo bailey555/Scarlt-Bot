aaf8351747efbe7791044b8f54f8532d3081c5e3
bailey555 <kookj5508@gmail.com>
2025-11-24 19:12:24 -0300

    Create pagar.js

---
+++ SeuBot/handlers/pagar.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getUser, saveDB } = require('../../handlers/dbManager');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('pagar')
+        .setDescription('Pague moedas a outro usuário')
+        .addUserOption(opt =>
+            opt.setName('usuário').setDescription('Quem vai receber').setRequired(true)
+        )
+        .addIntegerOption(opt =>
+            opt.setName('valor').setDescription('Quantidade').setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const author = getUser(interaction.user.id);
+        const targetUser = interaction.options.getUser('usuário');
+        const target = getUser(targetUser.id);
+        const amount = interaction.options.getInteger('valor');
+
+        if (amount <= 0)
+            return interaction.reply({ content: '⚠️ Valor inválido.', ephemeral: true });
+
+        if (author.money < amount)
+            return interaction.reply({ content: '❌ Você não tem dinheiro suficiente.', ephemeral: true });
+
+        author.money -= amount;
+        target.money += amount;
+        saveDB();
+
+        await interaction.reply(`💸 Você enviou **${amount} moedas** para **${targetUser.username}**.`);
+    }
+};
