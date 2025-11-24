1f63e1c931769352538c9405dc2a056fb435dbca
bailey555 <kookj5508@gmail.com>
2025-11-24 19:13:37 -0300

    Create saldo.js

---
+++ SeuBot/handlers/saldo.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getUser } = require('../../handlers/dbManager');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('saldo')
+        .setDescription('Veja seu saldo atual 💰'),
+
+    async execute(interaction) {
+        const user = getUser(interaction.user.id);
+
+        await interaction.reply(
+            `💰 **Carteira:** ${user.money}\n🏦 **Banco:** ${user.bank}`
+        );
+    }
+};
