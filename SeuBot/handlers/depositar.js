a07fc99e5117e0131b66dcfd2c8d11e49946de95
bailey555 <kookj5508@gmail.com>
2025-11-24 19:11:46 -0300

    Create depositar.js

---
+++ SeuBot/handlers/depositar.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getUser, saveDB } = require('../../handlers/dbManager');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('depositar')
+        .setDescription('Deposite dinheiro no banco')
+        .addIntegerOption(opt =>
+            opt.setName('valor').setDescription('Quantidade').setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const user = getUser(interaction.user.id);
+        const amount = interaction.options.getInteger('valor');
+
+        if (user.money < amount)
+            return interaction.reply({ content: '❌ Dinheiro insuficiente.', ephemeral: true });
+
+        user.money -= amount;
+        user.bank += amount;
+        saveDB();
+
+        await interaction.reply(`🏦 Você depositou **${amount} moedas**.`);
+    }
+};
